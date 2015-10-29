0a9758585c51160243e3cc3734179d03322e546a
tabinks <abinkowski@uchicago.edu>
2015-10-29 18:32:39 -0500

    Update multiprocessing_with_slurm.py

--- multiprocessing_with_slurm.py
+++ multiprocessing_with_slurm.py
@@ -1,46 +1,74 @@
 #!/bin/env python                                                                              
+################################################################################
+## SLURM variables
+################################################################################
 #SBATCH --job-name=multiprocess                                                                
 #SBATCH --output=multiprocess_%j.out                                                           
+## Sandy bridge has 16 cores per node, so we could run 16 processes
+#SBATCH --partition=sandyb
 #SBATCH --nodes=1                                                                              
+## Tell SLURM we want the entire node to ourselves
 #SBATCH --exclusive                                                                            
 
+################################################################################
+## Import the python libraries we will be using
+################################################################################
 import multiprocessing
 import sys
 import os
 import time
+import platform
 
 # necessary to add cwd to path when script run by slurm (since it executes a copy)                                                          
 sys.path.append(os.getcwd())
 
-##################################################################
-# get number of cpus available to job just for informational purposes                                                         
-##################################################################
+
+################################################################################
+# For debugging and profiling, get number of cpus available; this should match
+# 16 for sandbridge
+################################################################################
 try:
     ncpus = int(os.environ["SLURM_JOB_CPUS_PER_NODE"])
 except KeyError:
     ncpus = multiprocessing.cpu_count()
 
 ##################################################################
-## Substitute you sequence alignment function here
+## This is the function that will be run on each node.  The
+## the parameter is what is being passed in the array from `.map`.
+## You should substitute you sequence alignment function here and
+## use the paramter however best fits your application design.
 ##################################################################
-def workOnNode(database):
-    time.sleep(60) # Do something after arbitrary time
+def process_worker(database):
+    # Pretend there is something happending that takes 60 secsonds
+    time.sleep(60) 
+    
+    # Write output to file
+    outfile="output_%s.txt" % database
     f1=open(database, 'w+')
-    f1.write('This is a test using database' + database)
+    f1.write('This is a test using database %s on %s.' % (database,platform.node())
     print multiprocessing.current_process()
     f1.close()
 
 
 ##################################################################
-# Main
+## Main
 ##################################################################
 print("Number of CPUs: %d" % ncpus)
 start = time.time()
-# Change processes to what you want
-pool = multiprocessing.Pool(processes=2)
-# Notice that only the number of processes you select will be run at at a time.
-pool.map(workOnNode, ["db1", "db2","db3","db4","db5","db6","db7","db8","db9","db10","db11","db12","db13","db14","db15"])
-print "Parallel time: %f" % (time.time() - start)
+print("Start time: %s" % start)
+
+# Set the number of processes to the number available to us; this
+# isn't necessarily the best performing option.  You should test 
+# your application to determine this
+pool = multiprocessing.Pool(processes=ncpus)
+
+# The array of parameters that will be passed to your process worker
+database_chunks =  ["db1", "db2","db3","db4","db5","db6","db7","db8","db9","db10","db11","db12","db13","db14","db15","db16"]
+# Distribute the jobs to workers
+pool.map(process_worker,database_chunks)
+
+## More logging
+print "Run time: %f" % (time.time() - start)
 
 
 
