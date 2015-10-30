9855a80d90e933e663bfffe64a5090a598e4525f
tabinks <abinkowski@uchicago.edu>
2015-10-29 21:29:54 -0500

    Update multiprocessing_with_slurm.py

--- multiprocessing_with_slurm.py
+++ multiprocessing_with_slurm.py
@@ -1,77 +1,77 @@
-#!/bin/env python                                                                              
-################################################################################
-## SLURM variables
-################################################################################
-#SBATCH --job-name=multiprocess                                                                
-#SBATCH --output=multiprocess_%j.out                                                           
-## Sandy bridge has 16 cores per node, so we could run 16 processes
-#SBATCH --partition=sandyb
-#SBATCH --nodes=1                                                                              
-## Tell SLURM we want the entire node to ourselves
-#SBATCH --exclusive                                                                            
+#!/bin/env python                                                                                                                                                                                      
+################################################################################                                                                                                                       
+## SLURM variables                                                                                                                                                                                     
+################################################################################                                                                                                                       
+#SBATCH --job-name=multiprocess                                                                                                                                                                        
+#SBATCH --output=multiprocess_%j.out                                                                                                                                                                   
 
-################################################################################
-## Import the python libraries we will be using
-################################################################################
+## Sandy bridge has 16 cores per node, so we could run 16 processes                                                                                                                                    
+#SBATCH --partition=sandyb                                                                                                                                                                             
+#SBATCH --nodes=1                                                                                                                                                                                      
+#SBATCH --exclusive                                                                                                                                                                                    
+
+################################################################################                                                                                                                       
+## Import the python libraries we will be using                                                                                                                                                        
+################################################################################                                                                                                                       
 import multiprocessing
 import sys
 import os
 import time
 import platform
+import random
 
-# necessary to add cwd to path when script run by slurm (since it executes a copy)                                                          
+# necessary to add cwd to path when script run by slurm (since it executes a copy)                                                                                                                     
 sys.path.append(os.getcwd())
 
+################################################################################                                                                                                                       
+# For debugging and profiling, get number of cpus available; this should match                                                                                                                         
+# 16 for sandbridge                                                                                                                                                                                    
+################################################################################                                                                                                                       
+nodes = int(os.environ["SLURM_NNODES"])
+cpus = int(os.environ["SLURM_JOB_CPUS_PER_NODE"])
 
-################################################################################
-# For debugging and profiling, get number of cpus available; this should match
-# 16 for sandbridge
-################################################################################
-try:
-    ncpus = int(os.environ["SLURM_JOB_CPUS_PER_NODE"])
-except KeyError:
-    ncpus = multiprocessing.cpu_count()
+SLURM_NPROCS= nodes * cpus
 
-##################################################################
-## This is the function that will be run on each node.  The
-## the parameter is what is being passed in the array from `.map`.
-## You should substitute you sequence alignment function here and
-## use the paramter however best fits your application design.
-##################################################################
+##################################################################                                                                                                                                     
+## This is the function that will be run on each node.  The                                                                                                                                            
+## the parameter is what is being passed in the array from `.map`.                                                                                                                                     
+## You should substitute you sequence alignment function here and                                                                                                                                      
+## use the paramter however best fits your application design.                                                                                                                                         
+##################################################################                                                                                                                                     
 def process_worker(database):
-    # Log out the process
+    # Log out the process                                                                                                                                                                              
     print multiprocessing.current_process()
-    
-    # Pretend there is something happending that takes 60 secsonds
-    time.sleep(60) 
-    
-    # Write output to file
-    outfile="output_%s.txt" % database
-    f1=open(database, 'w+')
-    f1.write('This is a test using database %s on %s.' % (database,platform.node()))
+
+    # Pretend there is something happending that takes a random                                                                                                                                        
+    # amount of time between 5 and 120 seconds                                                                                                                                                         
+    time.sleep(random.randint(5,120))
+
+    # Write output to file                                                                                                                                                                             
+    outfile="output_%dx%d_%s.txt" % (nodes,tasks,database)
+    f1=open(outfile, 'w+')
+    f1.write('This is a test using the passed number %s on %s.\n' % (database,platform.node()))
     f1.close()
 
 
-##################################################################
-## Main
-##################################################################
-print("Number of CPUs: %d" % ncpus)
+##################################################################                                                                                                                                     
+## Main                                                                                                                                                                                                
+##################################################################                                                                                                                                     
+print("Number of Processes: %d" % SLURM_NPROCS)
 start = time.time()
 print("Start time: %s" % start)
 
-# Set the number of processes to the number available to us; this
-# isn't necessarily the best performing option.  You should test 
-# your application to determine this
-pool = multiprocessing.Pool(processes=ncpus)
+# Set the number of processes to the number available to us; this                                                                                                                                      
+# isn't necessarily the best performing option.  You should test                                                                                                                                       
+# your application to determine this                                                                                                                                                                   
+pool = multiprocessing.Pool(processes=SLURM_NPROCS)
 
-# The array of parameters that will be passed to your process worker
-database_chunks =  ["db1", "db2","db3","db4","db5","db6","db7","db8","db9","db10","db11","db12","db13","db14","db15","db16"]
-# Distribute the jobs to workers
+# The array of parameters that will be passed to your process worker                                                                                                                                   
+database_chunks =  [format(x,'02d') for x in range(0,100)]
+
+# Distribute the jobs to workers                                                                                                                                                                       
 pool.map(process_worker,database_chunks)
 
-## More logging
+## More logging                                                                                                                                                                                        
 print "Run time: %f" % (time.time() - start)
-
-
 
 
