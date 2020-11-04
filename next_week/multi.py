f85bea3620735b14c4a75b98f820ef51c8b9c098
Thomas Binkowski <abinkowski@midway2-login2.rcc.local>
2020-11-04 13:41:34 -0600

    Update and organize the examples

--- next_week/multi.py
+++ next_week/multi.py
@@ -1,30 +1,27 @@
-#!/bin/env python                                                                                                                                                                                      
-################################################################################                                                                                                                       
-## SLURM variables                                                                                                                                                                                   
-################################################################################                                                                                                        
-#SBATCH --job-name=multiprocess                                                                                                                                                  
-#SBATCH --output=%j_multiprocess.out                                                                                                                                                    
+#!/bin/env python
+
+## SLURM variables
+
+#SBATCH --job-name=multiprocess
+#SBATCH --output=%j_multiprocess.out
 #SBATCH --partition=broadwl
 
 #SBATCH --cpus-per-task=4   # cores
-#SBATCH --nodes=2           # number of nodes to run on       
+#SBATCH --nodes=4           # number of nodes to run on       
 #SBATCH --ntasks-per-node=4 # 
-#SBATCH --ntasks=8          # total tasks to be launcedd
+#SBATCH --ntasks=16          # total tasks to be launcedd
 
-#SBATCH --exclusive                                                                                                             
-                                                      
+#SBATCH--exclusive
 #SBATCH --time=00:05:00
 #SBATCH --account=mpcs56420
 
-################################################################################                                                                                                                       
-## Import the python libraries we will be using                                                                                                                                                        
-################################################################################                                                                                                                       
 import multiprocessing
 import sys
 import os
 import time
 import platform
 import random
+import socket
 
 # necessary to add cwd to path when script run 
 # by slurm (since it executes a copy)
@@ -47,45 +44,40 @@ print(os.environ)
 
 SLURM_NPROCS = nodes * cpus_per_task
 
-##################################################################                                                                                                                                    
-## This is the function that will be run on each node.  The                                                                                                                                            
-## the parameter is what is being passed in the array from `.map`.                                                                                                                                     
-## You should substitute you sequence alignment function here and                                                                                                                                      
-## use the paramter however best fits your application design.                                                                                                                                         
-##################################################################                                                                                                                                     
+## This is the function that will be run on each node.The
+## the parameter is what is being passed in the array from `.map`. 
+## You should substitute you sequence alignment function here and     
+## use the paramter however best fits your application design.
+
 def process_worker(database):
     # Log out the process                                                                                                                                                               
     print(multiprocessing.current_process())
-    # Pretend there is something happending that takes a random                                                                                                                       
-    # amount of time between 5 and 120 seconds                                                                                                                                                         
+    # Pretend there is something happending that takes a random amount of time between 5 and 120 seconds
     time.sleep(random.randint(5,120))
 
-    # Write output to file                                                                                                                                                                             
+    # Write output to file
     outfile="%s_output_%dx%d_%s.txt" % (job_id,nodes,cpus_per_task,database)
     f1=open(outfile, 'w+')
-    f1.write('This is a test using the passed number %s on %s.\n' % (database,platform.node()))
+    f1.write('This is a test using the passed number %s on %s %s.\n' % (database,platform.node(),socket.gethostname()))
     f1.close()
 
 
-##################################################################                                                                                                                                     
-## Main                                                                                                                                                                                                
-##################################################################                                                                                                                                     
-print("Number of Processes: %d" % SLURM_NPROCS)
-start = time.time()
-print("Start time: %s" % start)
+if __name__ == "__main__":
+    print("Number of Processes: %d" % SLURM_NPROCS)
+    start = time.time()
+    print("Start time: %s" % start)
 
-# Set the number of processes to the number available to us; this                                                                                                                                      
-# isn't necessarily the best performing option.  You should test                                                                                                                                       
-# your application to determine this                                                                                                                                                                   
-pool = multiprocessing.Pool(processes=SLURM_NPROCS)
+    # Set the number of processes to the number available to us; this isn't necessarily the best performing option.  You should test your application to determine this
 
-# The array of parameters that will be passed to your process worker.  
-# This creates array ["00","01"....]
-database_chunks =  [format(x,'02d') for x in range(0,10)]
+    pool = multiprocessing.Pool(processes=SLURM_NPROCS)
 
-# Distribute the jobs to workers                                                                                                                                                                       
-pool.map(process_worker,database_chunks)
+    # The array of parameters that will be passed to your process worker.  
+    # This creates array ["00","01"....]
+    database_chunks =  [format(x,'02d') for x in range(0,10)]
 
-print("Run time: %f" % (time.time() - start))
+    # Distribute the jobs to workers
+    pool.map(process_worker,database_chunks)
+
+    print("Run time: %f" % (time.time() - start))
 
 
